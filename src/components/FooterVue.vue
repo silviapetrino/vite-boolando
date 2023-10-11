ec7f4f8b2c3e7cb358d8d3fb3f35f1d6fb8fe2ef
silviapetrino <petrino.silvia@gmail.com>
2023-10-11 08:50:04 +0200

    [add]: html and style to footer

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -8,7 +8,46 @@ export default {
 </script>
 
 <template>
-    <h1>footer</h1>
+     <footer>
+    <div class="container display-flex">
+      <div class="text">
+        <h5><a href="">Booleando s.r.l</a></h5>
+        <nav>
+          <ul>
+          <li><a href="">Informazioni legali</a></li>
+          <li><a href="">Informativa di privacy</a></li>
+          <li><a href="">Diritto di recesso</a></li>
+        </ul>
+      </nav>
+      </div>
+      <div class="social">
+        <span>Trovaci anche su</span>
+        <nav>
+          <ul>
+          <li>
+            <a href=""><i class="fa-brands fa-square-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-square-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-square-instagram"></i></a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-square-pinterest"></i></a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-square-youtube"></i></a></li>
+        </ul>
+      </nav>
+      </div>
+      </div>
+
+  <!-- /footer  -->
+
+  </footer>
 </template>
 
 <style lang="scss" scoped>
