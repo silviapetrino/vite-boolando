c794cc7d112fa17fd298854156ca0a3f46e69b74
silviapetrino <petrino.silvia@gmail.com>
2023-10-11 15:01:02 +0200

    [add]: static card with scss re-style

--- src/components/MainVue.vue
+++ src/components/MainVue.vue
@@ -7,11 +7,35 @@ export default {
 </script>
 
 <template>
-    <h1>main</h1>
+    
+    <div class="container display-flex">
+
+        <div class="card">
+            <img src="../../public/img/1.webp" alt="uomo">
+
+            <span class="heart">&hearts;</span>
+            <div class="marks">
+                <span class="range-discount">-50%</span>
+                <span class="value">Sostenibilità</span>
+            </div>
+
+            <div class="product-details">
+                <span class="brand">Levi's</span>
+                <h4 class="product-name">RELAXED FIT TEE UNISEX</h4>
+                <span class="price">14,99&euro</span>
+                <span class="discount">29,99&euro</span>
+            </div>
+      </div>
+
+    </div>
+        
+    
+
+
 </template>
 
 <style lang="scss" scoped>
 
-@use '../scss/partials/middle.scss'
+@use '../scss/partials/cardStyle.scss'
 
 </style>
